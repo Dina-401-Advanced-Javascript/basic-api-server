3a35a7bc3ff4e27e2c7f542b5d4ab51c369b5c30
Dina Ayoub <dinaayoub@hotmail.com>
2020-12-10 22:35:32 -0800

    got online mongo db qworking! hurray!

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -2,7 +2,7 @@ function validator(req, res, next) {
 
   if (req.method === 'PUT' || req.method === 'DELETE') {
     //make sure an id is provided
-    console.log(req.params.id);
+    //console.log(req.params.id);
     if (req.params.id) next();
     else next('No ID provided');
   }
